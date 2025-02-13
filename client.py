1630dd98e148ecd3f828e134876a6b3358ef87d1
actuallypav <professional.extras708@passinbox.com>
2025-02-13 10:53:27 +0000

    exception handling

--- client.py
+++ client.py
@@ -1,25 +1,35 @@
 import requests
 from requests.auth import HTTPDigestAuth
-
+from requests.exceptions import ConnectionError, Timeout
+from requests.models import Response
 
 # set variables as to who to connect to
-# TODO: make it so user can input the values... maybe use a basic tkinter?
 url = "http://127.0.0.1:5001"
 auth = HTTPDigestAuth("admin", "123abc")
+response = Response()
+response.status_code = 400 
 
 #input logic for auth
-print("Connecting to {1}", url)
+print(f"Connecting to {url}")
 print("Enter your Username: ")
 username = input()
 print("Enter your Password: ")
 password = input()
 auth = HTTPDigestAuth(username, password)
 
-# see what the response from the server is
-response = requests.get(url, auth=auth)
+try:
+    # see what the response from the server is
+    response = requests.get(url, auth=auth)
+except ConnectionError:
+    print("Error: Unable to connect to the server. Is it still running?")
+except Timeout:
+    print("Errorr The request timed out. Try again later.")
+except requests.HTTPError as http_err:
+    print(f"HTTP Error occurred: {http_err}")
+except Exception as err:
+    print(f"An error occurred: {err}")
 
 # check if we got in
 if response.status_code == 200:
     print(response.text)
-else:
-    print(f"Error: {response.status_code}")
+
