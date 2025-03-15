42d8e3af00946e4e398550f11ae2c6242cbb5c02
actuallypav <professional.extras708@passinbox.com>
2025-03-15 19:07:46 +0000

    fmt

--- client/client.py
+++ client/client.py
@@ -11,76 +11,87 @@ import boto3
 import json
 import requests
 
+
 def parse_tfvars(file_path):
     variables = {}
     with open(file_path, "r") as file:
         for line in file:
             line = line.strip()
             if line and not line.startswith("#"):
-                key,value = map(str.strip, line.split("=", 1))
+                key, value = map(str.strip, line.split("=", 1))
                 variables[key] = value.strip('"')
     return variables["region"], variables["kv_name"]
 
+
 def generate_csr():
     private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    
-    csr = x509.CertificateSigningRequestBuilder().subject_name(
-        x509.Name([
-            x509.NameAttribute(x509.oid.NameOID.COUNTRY_NAME, u"UK"),
-            x509.NameAttribute(x509.oid.NameOID.STATE_OR_PROVINCE_NAME, u"Cumbria"),
-            x509.NameAttribute(x509.oid.NameOID.LOCALITY_NAME, u"Keswick"),
-            x509.NameAttribute(x509.oid.NameOID.ORGANIZATION_NAME, u"PavInc"),
-            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, u"github.com/actuallypav"),
-        ])
-    ).sign(private_key, hashes.SHA256())
+
+    csr = (
+        x509.CertificateSigningRequestBuilder()
+        .subject_name(
+            x509.Name(
+                [
+                    x509.NameAttribute(x509.oid.NameOID.COUNTRY_NAME, "UK"),
+                    x509.NameAttribute(
+                        x509.oid.NameOID.STATE_OR_PROVINCE_NAME, "Cumbria"
+                    ),
+                    x509.NameAttribute(x509.oid.NameOID.LOCALITY_NAME, "Keswick"),
+                    x509.NameAttribute(x509.oid.NameOID.ORGANIZATION_NAME, "PavInc"),
+                    x509.NameAttribute(
+                        x509.oid.NameOID.COMMON_NAME, "github.com/actuallypav"
+                    ),
+                ]
+            )
+        )
+        .sign(private_key, hashes.SHA256())
+    )
 
     with open("private_key.pem", "wb") as key_file:
-        key_file.write(private_key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        ))
-    
+        key_file.write(
+            private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        )
+
     return csr.public_bytes(serialization.Encoding.PEM)
+
 
 def encrypt_aes256(data, key, iv):
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     encryptor = cipher.encryptor()
-    
+
     # PKCS7 Padding to make data a multiple of 16 bytes
     pad_len = 16 - (len(data) % 16)
     data += bytes([pad_len] * pad_len)
-    
+
     encrypted_data = encryptor.update(data) + encryptor.finalize()
     return encrypted_data
 
+
 def retrieve_kv(region, kv_name):
-    sm = boto3.client('secretsmanager')
-    account_id = boto3.client('sts').get_caller_identity().get('Account')
+    sm = boto3.client("secretsmanager")
+    account_id = boto3.client("sts").get_caller_identity().get("Account")
 
     arn = f"arn:aws:secretsmanager:{region}:{account_id}:secret:{kv_name}"
 
-    kv = sm.get_secret_value(
-        SecretId=arn
-    )
+    kv = sm.get_secret_value(SecretId=arn)
 
     kv_dict = json.loads(kv["SecretString"])
 
-    #b64 decode all
+    # b64 decode all
     key = base64.b64decode(kv["aes_key"])
     iv = base64.b64decode(kv["aes_iv"])
 
     return key, iv
 
+
 def get_pem(csr):
     api_gateway_url = ""
 
-    headers = {
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "csr": csr
-    }
+    headers = {"Content-Type": "application/json"}
+    payload = {"csr": csr}
 
     response = requests.post(api_gateway_url, json=payload, headers=headers)
 
@@ -90,24 +101,25 @@ def get_pem(csr):
         print(f"ERROR: {response.status_code}, {response.text}")
         return {}
 
-def main():    
+
+def main():
     # generate CSR
     csr_data = generate_csr()
 
-    #recover key/iv from secrets
+    # recover key/iv from secrets
     region, kv_name = parse_tfvars("../terraform.tfvars")
     key, iv = retrieve_kv(region, kv_name)
 
     # encrypt CSR
     encrypted_csr = encrypt_aes256(csr_data, key, iv)
-    
+
     # encode with Base64
     b64_encoded_csr = base64.b64encode(encrypted_csr).decode()
-    
+
     response = get_pem(b64_encoded_csr).get("body", "")
     response_json = response.json()
-    root_ca = base64.b64decode(response_json.get("root_ca","")).decode()
-    cert_pem = base64.b64decode(response_json.get("cert_pem","")).decode()
+    root_ca = base64.b64decode(response_json.get("root_ca", "")).decode()
+    cert_pem = base64.b64decode(response_json.get("cert_pem", "")).decode()
 
     with open("root_ca.pem", "w") as r:
         r.write(root_ca)
@@ -116,6 +128,7 @@ def main():
         c.write(cert_pem)
 
     print("Success find the certificates here!")
-    
+
+
 if __name__ == "__main__":
     main()
--- src/server.py
+++ src/server.py
@@ -9,22 +9,23 @@ import requests
 import boto3
 import os
 
+
 def lambda_handler(event, context):
     try:
-        source_ip = event['requestContext']['http']['sourceIp']
+        source_ip = event["requestContext"]["http"]["sourceIp"]
 
         kv_name = os.environ.get["KV_NAME"]
         region = os.environ.get["REGION"]
-        root_ca_url =  os.environ.get["ROOT_CA_URL"]
+        root_ca_url = os.environ.get["ROOT_CA_URL"]
 
-        account_id = boto3.client('sts').get_caller_identity().get('Account')
+        account_id = boto3.client("sts").get_caller_identity().get("Account")
 
         if "body" not in event or not event["body"]:
             return {
                 "statusCode": 400,
-                "body": json.dumps({"error": "No CSR data received"})
+                "body": json.dumps({"error": "No CSR data received"}),
             }
-        
+
         csr_aes = base64.b64decode(event["body"])
 
         csr_pem = decrypt_csr(csr_aes, account_id, region, kv_name).encode()
@@ -34,46 +35,37 @@ def lambda_handler(event, context):
 
         root_ca = download_root_ca(root_ca_url)
         cert_pem = sign_csr(csr)
-        
-        #send the signed stuff back to the client
 
-        response_data = {
-            "root_ca": root_ca,
-            "cert_pem": cert_pem
-        }
+        # send the signed stuff back to the client
+
+        response_data = {"root_ca": root_ca, "cert_pem": cert_pem}
 
         return {
             "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": base64.b64encode(json.dumps(response_data).encode())
+            "headers": {"Content-Type": "application/json"},
+            "body": base64.b64encode(json.dumps(response_data).encode()),
         }
     except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"ERROR": str(e)})
-        }
+        return {"statusCode": 500, "body": json.dumps({"ERROR": str(e)})}
+
 
 def decrypt_csr(ciphertext, account_id, region, kv_name):
-    #retrive key/iv
-    sm = boto3.client('secretsmanager')
+    # retrive key/iv
+    sm = boto3.client("secretsmanager")
 
     arn = f"arn:aws:secretsmanager:{region}:{account_id}:secret:{kv_name}"
 
-    kv = sm.get_secret_value(
-        SecretId=arn
-    )
+    kv = sm.get_secret_value(SecretId=arn)
 
     kv_dict = json.loads(kv["SecretString"])
 
-    #b64 decode all
+    # b64 decode all
     key = base64.b64decode(kv["aes_key"])
     iv = base64.b64decode(kv["aes_iv"])
 
     ciphertext = base64.b64decode(ciphertext)
 
-    #decrypt with AES
+    # decrypt with AES
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     decryptor = cipher.decryptor()
     plaintext_padded = decryptor.update(plaintext_padded) + decryptor.finalize()
@@ -83,9 +75,10 @@ def decrypt_csr(ciphertext, account_id, region, kv_name):
 
     return plaintext.decode()
 
+
 def verify_csr(csr):
     public_key = csr.public_key()
-    
+
     try:
         public_key.verify(
             csr.signature,
@@ -96,9 +89,10 @@ def verify_csr(csr):
         print("CSR is valid")
     except Exception as e:
         print("CSR verification failed ", e)
-    
-        #TODO: Return error to the client - for now just quit lambda
+
+        # TODO: Return error to the client - for now just quit lambda
         raise Exception("Forced Lambda exit - can't verify CSR")
+
 
 def download_root_ca(ca_url):
     response = requests.get(ca_url)
@@ -106,26 +100,29 @@ def download_root_ca(ca_url):
         return response.content
     else:
         print("Error retriving Amazon Root CA")
-        #TODO: Return error to the client - for now just quit lambda
+        # TODO: Return error to the client - for now just quit lambda
         raise Exception("Forced Lambda exit - can't verify CSR")
 
+
 def sign_csr(csr):
-    #sign csr
+    # sign csr
     iot = boto3.client("iot")
 
     response = iot.create_certificate_from_csr(
-        certifacteSigningRequest=csr.public_bytes(encoding=serialization.Encoding.PEM).decode(),
-        setAsActive=True
+        certifacteSigningRequest=csr.public_bytes(
+            encoding=serialization.Encoding.PEM
+        ).decode(),
+        setAsActive=True,
     )
 
     cert_arn = response["certificateARN"]
     cert_pem = response["certificatePem"]
 
-    #TODO: Retrive name/policies from OID for now just make default ones
+    # TODO: Retrive name/policies from OID for now just make default ones
     thing_name = "IoTPavTest"
     policy_name = "IoTPavTestPolicy"
 
-    #Verify the above exists - if not create
+    # Verify the above exists - if not create
     try:
         iot.describe_thing(thingName=thing_name)
         print(f"Thing '{thing_name}' exists.")
@@ -141,26 +138,15 @@ def sign_csr(csr):
         policy_document = {
             "Version": "2012-10-17",
             "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": "iot:Connect",
-                    "Resource": "*"
-                }
-            ]
+                {"Effect": "Allow", "Action": "iot:Connect", "Resource": "*"}
+            ],
         }
         iot.create_policy(
-            policyName=policy_name,
-            policyDocument=json.dumps(policy_document)
+            policyName=policy_name, policyDocument=json.dumps(policy_document)
         )
 
-    iot.attach_thing_principal(
-        thingName=thing_name,
-        principal=cert_arn
-    )
+    iot.attach_thing_principal(thingName=thing_name, principal=cert_arn)
 
-    iot.attach_policy(
-        policyName=policy_name,
-        target=cert_arn
-    )
+    iot.attach_policy(policyName=policy_name, target=cert_arn)
 
     return cert_pem
